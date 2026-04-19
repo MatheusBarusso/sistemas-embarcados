1d1f1f5ae94eba8e182e49075b734adebe681c05
MatheusBarusso <matheusbbarusso@gmail.com>
2026-04-19 19:50:05 -0300

    SEGA Genesis to HID Working  (3 btn only)

--- RAW_GEN_to_HID/RAW_GEN_to_HID.ino
+++ RAW_GEN_to_HID/RAW_GEN_to_HID.ino
@@ -1,3 +1,8 @@
+#include "USB.h"
+#include "USBHIDGamepad.h"
+
+USBHIDGamepad gamepad;
+
 #define PIN_UP     4
 #define PIN_DOWN   5
 #define PIN_LEFT   6
@@ -7,8 +12,6 @@
 #define PIN_SEL    16
 
 void setup() {
-  Serial.begin(115200);
-
   pinMode(PIN_UP, INPUT);
   pinMode(PIN_DOWN, INPUT);
   pinMode(PIN_LEFT, INPUT);
@@ -17,12 +20,16 @@ void setup() {
   pinMode(PIN_C, INPUT);
 
   pinMode(PIN_SEL, OUTPUT);
+
+  USB.begin();
+  gamepad.begin();
 }
 
 void loop() {
   bool up, down, left, right;
   bool a, b, c, start;
-
+  
+  // Leitura com SEL HIGH (Lê Cima, Baixo, Esquerda, Direita, B e C)
   digitalWrite(PIN_SEL, HIGH);
   delayMicroseconds(50);
 
@@ -33,21 +40,43 @@ void loop() {
   b     = !digitalRead(PIN_B);
   c     = !digitalRead(PIN_C);
 
-  
+  // Leitura com SEL LOW (Lê A e Start nos pinos correspondentes a B e C)
   digitalWrite(PIN_SEL, LOW);
   delayMicroseconds(50);
 
   a     = !digitalRead(PIN_B);
   start = !digitalRead(PIN_C);
 
-  Serial.print("U:"); Serial.print(up);
-  Serial.print(" D:"); Serial.print(down);
-  Serial.print(" L:"); Serial.print(left);
-  Serial.print(" R:"); Serial.print(right);
-  Serial.print(" A:"); Serial.print(a);
-  Serial.print(" B:"); Serial.print(b);
-  Serial.print(" C:"); Serial.print(c);
-  Serial.print(" START:"); Serial.println(start);
 
-  delay(100);
+  // Mapeamento de Botões (Bitmask)
+  uint32_t buttons = 0; 
+  if (a) buttons |= (1 << 0);     // Botão 1 (A)
+  if (b) buttons |= (1 << 1);     // Botão 2 (B)
+  if (c) buttons |= (1 << 2);     // Botão 3 (C)
+  if (start) buttons |= (1 << 7); // Botão 8 (Start)
+
+  // Mapeamento do D-PAD (HAT)
+  uint8_t hat = 0; // 0 = Centro
+
+
+  if (up) hat = 1;
+  if (down) hat = 5;
+  if (left) hat = 7;
+  if (right) hat = 3;
+
+
+  if (up && right) hat = 2;
+  if (down && right) hat = 4;
+  if (down && left) hat = 6;
+  if (up && left) hat = 8;
+
+
+  gamepad.send(
+    0, 0, 0,   
+    0, 0, 0,   
+    hat,       
+    buttons    
+  );
+
+  delay(10);
 }
